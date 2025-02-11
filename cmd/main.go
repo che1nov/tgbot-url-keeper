38436b6157fe793a04ab73de761df720948299e5
Ilya Chernov <ilya.chernov1@icloud.com>
2025-02-11 06:17:57 +0400

    v.1.3

--- cmd/main.go
+++ cmd/main.go
@@ -9,12 +9,6 @@ import (
 )
 
 func main() {
-	//if err := godotenv.Load(); err != nil {
-	//	log.Println("Warning: No .env file found, using system environment variables")
-	//} else {
-	//	log.Println(".env file loaded successfully")
-	//}
-
 	if err := storage.Init(); err != nil {
 		log.Fatalf("Failed to initialize storage: %v", err)
 	}
